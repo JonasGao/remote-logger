cc0e63839c1ff3627c65c75443ded4768ad1a808
JonasGao <jonas.gao@voyageone.cn>
2018-07-04 10:54:57 +0800

    ok, lets do it

---
+++ app.py
@@ -0,0 +1,26 @@
+#!remote-logger/bin/python
+from flask import Flask, request, abort, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/log', methods=['POST'])
+def add_record():
+    if not request.json or 'content' not in request.json:
+        abort(400)
+    print request.json['content']
+    return 'OK', 201
+
+
+@app.errorhandler(400)
+def bad_req_body(error):
+    return jsonify({'message': 'No content!'}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
