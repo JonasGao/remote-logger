f7432effa8ddd831d9f00797ffc3e0cbd90fca49
JonasGao <gaopinsong@live.cn>
2019-03-21 21:40:16 +0800

    prepare v2

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8888;
+const socket = require('socket.io');
 
 app.use(express.static(__dirname + '/public'));
 
-function onConnection(socket){
+const legecyIOOption = {
+  transports: ['websocket'],
+  serveClient: false
+}
+const legecyIO = socket(http, legecyIOOption);
+const legecyOnConnection = socket => {
   socket.on('log', (data) => console.log(data));
 }
+legecyIO.on('connection', legecyOnConnection);
 
-io.on('connection', onConnection);
+const v2IOOption = {
+  path: '/socket.io/v2',
+  serveClient: false,
+  transports: ['websocket']
+};
+const v2IO = socket(http, v2IOOption);
+const v2OnConnection = socket => {
+  socket.on('log', data => {
+    console.log(data);
+  });
+};
+v2IO.on('connection', v2OnConnection);
 
 http.listen(port, () => console.log('listening on port ' + port))
