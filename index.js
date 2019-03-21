2702761f8e54e2590fe9d5fcd56a17719301b3db
JonasGao <gaopinsong@live.cn>
2019-03-21 22:11:54 +0800

    update handler

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
+const http = require("http").Server(app);
 const port = process.env.PORT || 8888;
-const socket = require('socket.io');
+const socket = require("socket.io");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-const legecyIOOption = {
-  transports: ['websocket'],
+const v1Handler = data => {
+  console.log(data);
+  return true;
+};
+const v2Handler = data => {
+  if (data.v && data.v == 2) {
+    // TODO: start here
+    console.log('v2!', data)
+    return true;
+  }
+  return false;
+};
+
+const logHandler = data => {
+  v2Handler(data) || v1Handler(data);
+};
+const ioOption = {
+  transports: ["websocket"],
   serveClient: false
-}
-const legecyIO = socket(http, legecyIOOption);
-const legecyOnConnection = socket => {
-  socket.on('log', (data) => console.log(data));
-}
-legecyIO.on('connection', legecyOnConnection);
-
-const v2IOOption = {
-  path: '/socket.io/v2',
-  serveClient: false,
-  transports: ['websocket']
 };
-const v2IO = socket(http, v2IOOption);
-const v2OnConnection = socket => {
-  socket.on('log', data => {
-    console.log(data);
-  });
+const io = socket(http, ioOption);
+const onConnection = socket => {
+  socket.on("log", logHandler);
 };
-v2IO.on('connection', v2OnConnection);
+io.on("connection", onConnection);
 
-http.listen(port, () => console.log('listening on port ' + port))
+http.listen(port, () => console.log("listening on port " + port));
