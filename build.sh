aa156198421d07bc87f89ebff96ea2edfb2285ee
Jonas <jonas.gao@voyageone.cn>
2019-03-25 10:17:14 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-tar -czvf remote-logger.tar.gz public handlers config.js handler.js index.js startup.sh 
+tar -czvf remote-logger.tar.gz public handlers config.js handler.js index.js startup.sh package.json 
