a5687abd67ef22de984ffbac98571f906b5cf310
Jonas <jonas.gao@voyageone.cn>
2019-03-25 10:48:20 +0800

    update for node 8

--- handlers/appFileHandler.js
+++ handlers/appFileHandler.js
@@ -1,24 +1,31 @@
 const fs = require("fs");
 const path = require("path");
-const endOfLine = require('os').EOL;
+const endOfLine = require("os").EOL;
 const logPath = require("../config").logPath;
 
 const mkdir = filename => {
   const { dir } = path.parse(filename);
   return new Promise((resolve, reject) => {
-    fs.mkdir(dir, { recursive: true }, err=>{
-      err ? reject(err) : resolve()
+    fs.access(dir, fs.constants.F_OK, err => {
+      if (err) {
+        console.log("access error, try mkdir. [error]", err);
+        fs.mkdir(dir, { recursive: true }, err => {
+          err ? reject(err) : resolve();
+        });
+        return;
+      }
+      resolve();
     });
-  })
+  });
 };
 
 const appendFile = async (path, data) => {
   await mkdir(path);
   return new Promise((resolve, reject) => {
     fs.appendFile(path, data + endOfLine, err => {
-      err ? reject(err) : resolve()
+      err ? reject(err) : resolve();
     });
-  })
+  });
 };
 
 const getAppLogFileName = appName => {
