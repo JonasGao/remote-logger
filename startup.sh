afa241f01eb32cd766ea9a8d88d99fd7ed4289c6
Jonas <jonas.gao@voyageone.cn>
2019-03-25 10:19:01 +0800

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 LOG_PATH=$1
 
 if [ -z "$LOG_PATH" ]; then
-  LOG_PATH="./logs"
-fi
+          LOG_PATH="./logs"
+          fi
 
 # /data/app_hs/var/log/remote-log
 nohup node index.js > "$LOG_PATH/output" 2>&1 &
 echo $! > pid
 echo "PID: [$(cat pid)]"
+
